f89501663fd77d0687b6a684ce6d3d7693eb142a
Ash Lai <z0975201508@gmail.com>
2022-10-06 10:56:17 +0800

    fix: adjust vueconfig publicPath to repository name.

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.VUE_APP_ENV === 'dev' || process.env.VUE_APP_ENV === 'local' ? '/' : '/DIMSWEB'
+    publicPath: process.env.VUE_APP_ENV === 'dev' || process.env.VUE_APP_ENV === 'local' ? '/esun-demo' : '/DIMSWEB'
 }
